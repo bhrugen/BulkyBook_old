563e0bce42c3454aeb232e148209599ef9d64320
Ben Spark <bhrugen1990@gmail.com>
2019-12-31 15:44:18 -0600

    Section 8 - Add Stored Procedure for Cover Type

---
+++ BulkyBook.DataAccess/Migrations/20191231213959_AddStoredProcForCoverType.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BulkyBook.DataAccess.Migrations
+{
+    public partial class AddStoredProcForCoverType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE PROC usp_GetCoverTypes 
+                                    AS 
+                                    BEGIN 
+                                     SELECT * FROM   dbo.CoverTypes 
+                                    END");
+
+            migrationBuilder.Sql(@"CREATE PROC usp_GetCoverType 
+                                    @Id int 
+                                    AS 
+                                    BEGIN 
+                                     SELECT * FROM   dbo.CoverTypes  WHERE  (Id = @Id) 
+                                    END ");
+
+            migrationBuilder.Sql(@"CREATE PROC usp_UpdateCoverType
+	                                @Id int,
+	                                @Name varchar(100)
+                                    AS 
+                                    BEGIN 
+                                     UPDATE dbo.CoverTypes
+                                     SET  Name = @Name
+                                     WHERE  Id = @Id
+                                    END");
+
+            migrationBuilder.Sql(@"CREATE PROC usp_DeleteCoverType
+	                                @Id int
+                                    AS 
+                                    BEGIN 
+                                     DELETE FROM dbo.CoverTypes
+                                     WHERE  Id = @Id
+                                    END");
+
+            migrationBuilder.Sql(@"CREATE PROC usp_CreateCoverType
+                                   @Name varchar(100)
+                                   AS 
+                                   BEGIN 
+                                    INSERT INTO dbo.CoverTypes(Name)
+                                    VALUES (@Name)
+                                   END");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP PROCEDURE usp_GetCoverTypes");
+            migrationBuilder.Sql(@"DROP PROCEDURE usp_GetCoverType");
+            migrationBuilder.Sql(@"DROP PROCEDURE usp_UpdateCoverType");
+            migrationBuilder.Sql(@"DROP PROCEDURE usp_DeleteCoverType");
+            migrationBuilder.Sql(@"DROP PROCEDURE usp_CreateCoverType");
+        }
+    }
+}
