d4fe543cdcf3846c6ca20dc62fd255683778451f
Ben Spark <bhrugen1990@gmail.com>
2019-12-31 15:15:49 -0600

    Section 7 - Category Delete Js

--- BulkyBook/wwwroot/js/category.js
+++ BulkyBook/wwwroot/js/category.js
@@ -20,7 +20,7 @@ function loadDataTable() {
                                 <a href="/Admin/Category/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
                                     <i class="fas fa-edit"></i> 
                                 </a>
-                                <a  class="btn btn-danger text-white" style="cursor:pointer">
+                                <a onclick=Delete("/Admin/Category/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
                                     <i class="fas fa-trash-alt"></i> 
                                 </a>
                             </div>
@@ -28,5 +28,31 @@ function loadDataTable() {
                 }, "width": "40%"
             }
         ]
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to Delete?",
+        text: "You will not be able to restore the data!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
     });
 }
