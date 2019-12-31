eb351febfb3d68ea3bba083f47bffa9cedbf27e7
Ben Spark <bhrugen1990@gmail.com>
2019-12-30 23:08:55 -0600

    Section 6 - Repository Interface

---
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T :class
+    {
+        T Get(int id);
+
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+            );
+
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filter = null,
+            string includeProperties = null
+            );
+
+        void Add(T entity);
+        void Remove(int id);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+
+
+    }
+}
