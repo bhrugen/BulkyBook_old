03275f89fa8a7335142eaeddc7d3d60262b373bb
Ben Spark <bhrugen1990@gmail.com>
2019-12-31 11:07:11 -0600

    Section 7 - Category js

---
+++ BulkyBook/wwwroot/js/category.js
@@ -0,0 +1,32 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Category/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "60%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                            <div class="text-center">
+                                <a href="/Admin/Category/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-edit"></i> 
+                                </a>
+                                <a  class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fas fa-trash-alt"></i> 
+                                </a>
+                            </div>
+                           `;
+                }, "width": "40%"
+            }
+        ]
+    });
+}
