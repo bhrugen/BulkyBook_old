fa084e6435fedcd5b3e671900ebec3210ede4326
Ben Spark <bhrugen1990@gmail.com>
2019-12-31 15:10:19 -0600

    Section 7 - Category Delete API Call

--- BulkyBook/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -72,6 +72,20 @@ namespace BulkyBook.Areas.Admin.Controllers
             return Json(new { data = allObj });
         }
 
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Category.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+
+        }
+
         #endregion
     }
 }
