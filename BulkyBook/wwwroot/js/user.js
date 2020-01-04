47d351b8e124158d843a95de38caeb820ee4b4dd
Ben Spark <bhrugen1990@gmail.com>
2020-01-04 01:25:44 -0600

    Section 12 - Final Lock Unlock API Call

--- BulkyBook/wwwroot/js/user.js
+++ BulkyBook/wwwroot/js/user.js
@@ -27,7 +27,7 @@ function loadDataTable() {
                         //user is currently locked
                         return `
                             <div class="text-center">
-                                <a onclick=LockUnlock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer">
+                                <a onclick=LockUnlock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
                                     <i class="fas fa-lock-open"></i>  Unlock
                                 </a>
                             </div>
@@ -36,7 +36,7 @@ function loadDataTable() {
                     else {
                         return `
                             <div class="text-center">
-                                <a onclick=LockUnlock('${data.id}') class="btn btn-success text-white" style="cursor:pointer">
+                                <a onclick=LockUnlock('${data.id}') class="btn btn-success text-white" style="cursor:pointer; width:100px;">
                                     <i class="fas fa-lock"></i>  Lock
                                 </a>
                             </div>
@@ -49,18 +49,13 @@ function loadDataTable() {
     });
 }
 
-function Delete(url) {
-    swal({
-        title: "Are you sure you want to Delete?",
-        text: "You will not be able to restore the data!",
-        icon: "warning",
-        buttons: true,
-        dangerMode: true
-    }).then((willDelete) => {
-        if (willDelete) {
+function LockUnlock(id) {
+    
             $.ajax({
-                type: "DELETE",
-                url: url,
+                type: "POST",
+                url: '/Admin/User/LockUnlock',
+                data: JSON.stringify(id),
+                contentType: "application/json",
                 success: function (data) {
                     if (data.success) {
                         toastr.success(data.message);
@@ -71,6 +66,5 @@ function Delete(url) {
                     }
                 }
             });
-        }
-    });
+      
 }
