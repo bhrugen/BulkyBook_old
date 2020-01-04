5f3239895a14eb9347923ef6775c899f48f2437f
Ben Spark <bhrugen1990@gmail.com>
2020-01-04 13:43:34 -0600

    Section 14 - Send Email

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,21 +11,23 @@ namespace BulkyBook.Utility
 {
     public class EmailSender : IEmailSender
     {
+        private readonly EmailOptions emailOptions;
+
+        public EmailSender(IOptions<EmailOptions> options)
+        {
+            emailOptions = options.Value;
+        }
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+            return Execute(emailOptions.SendGridKey, subject, htmlMessage, email);
         }
-        static async Task Execute()
+        private Task Execute(string sendGridKEy, string subject,string message, string email)
         {
-            var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("test@example.com", "Example User");
-            var subject = "Sending with SendGrid is Fun";
-            var to = new EmailAddress("test@example.com", "Example User");
-            var plainTextContent = "and easy to do anywhere, even with C#";
-            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);
+            var client = new SendGridClient(sendGridKEy);
+            var from = new EmailAddress("admin@bulky.com", "Bulky Books");
+            var to = new EmailAddress(email, "End User");
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", message);
+            return client.SendEmailAsync(msg);
         }
     }
 }
