603c5370e466820e37ed716e24c8146d419ccafe
Ben Spark <bhrugen1990@gmail.com>
2020-01-04 14:52:51 -0600

    Section 15 - Shopping Cart Model

---
+++ BulkyBook.Models/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+            Count = 1;
+        }
+        [Key]
+        public int Id { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public string ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+
+        [Range(1,1000,ErrorMessage ="Please enter a value between 1 and 1000")]
+        public int Count { get; set; }
+
+        [NotMapped]
+        public double Price { get; set; }
+
+    }
+}
