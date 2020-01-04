9e3d10721f95c4f2dcbe8a4a6555dc99315a892d
Ben Spark <bhrugen1990@gmail.com>
2020-01-04 11:06:47 -0600

    Section 13 - Sign in with Facebook

--- BulkyBook/Startup.cs
+++ BulkyBook/Startup.cs
@@ -46,6 +46,11 @@ namespace BulkyBook
                 options.LogoutPath = $"/Identity/Account/Logout";
                 options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
             });
+            services.AddAuthentication().AddFacebook(options =>
+            {
+                options.AppId = "479144716347128";
+                options.AppSecret = "8888cefba55e9cfa06a2b28f0495e533";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
