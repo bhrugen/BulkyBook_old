5c7cbc9e8f13678e31aa9ecd767f3de14bad120c
Ben Spark <bhrugen1990@gmail.com>
2020-01-04 16:54:40 -0600

    Section 16 - Configure Session in the Project

--- BulkyBook/Startup.cs
+++ BulkyBook/Startup.cs
@@ -58,6 +58,12 @@ namespace BulkyBook
                 options.ClientSecret = "LPRLug47n8OQsYAirUVGofLw";
 
             });
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -78,7 +84,7 @@ namespace BulkyBook
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseSession();
             app.UseAuthentication();
             app.UseAuthorization();
 
