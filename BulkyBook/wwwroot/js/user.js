10f5f3189a05e711ba6ef0cfffeb92ac16dd6946
Ben Spark <bhrugen1990@gmail.com>
2020-01-04 01:14:37 -0600

    Section 12 - User Lock Unlock JS

--- BulkyBook/wwwroot/js/user.js
+++ BulkyBook/wwwroot/js/user.js
@@ -15,22 +15,36 @@ function loadDataTable() {
             { "data": "email", "width": "15%" },
             { "data": "phoneNumber", "width": "15%" },
             { "data": "company.name", "width": "15%" },
-            { "data": "role", "width": "15%" }
-            //{
-            //    "data": "id",
-            //    "render": function (data) {
-            //        return `
-            //                <div class="text-center">
-            //                    <a href="/Admin/Category/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
-            //                        <i class="fas fa-edit"></i> 
-            //                    </a>
-            //                    <a onclick=Delete("/Admin/Category/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
-            //                        <i class="fas fa-trash-alt"></i> 
-            //                    </a>
-            //                </div>
-            //               `;
-            //    }, "width": "40%"
-            //}
+            { "data": "role", "width": "15%" },
+            {
+                "data": {
+                    id: "id", lockoutEnd: "lockoutEnd"
+                },
+                "render": function (data) {
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
+                    if (lockout > today) {
+                        //user is currently locked
+                        return `
+                            <div class="text-center">
+                                <a onclick=LockUnlock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fas fa-lock-open"></i>  Unlock
+                                </a>
+                            </div>
+                           `;
+                    }
+                    else {
+                        return `
+                            <div class="text-center">
+                                <a onclick=LockUnlock('${data.id}') class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-lock"></i>  Lock
+                                </a>
+                            </div>
+                           `;
+                    }
+                    
+                }, "width": "25%"
+            }
         ]
     });
 }
