a0d1d4ad17e9d366e028b028937cf3c67dba8e8d
Ben Spark <bhrugen1990@gmail.com>
2020-01-05 03:07:04 -0600

    Section 17 - Cart UI Part 1

--- BulkyBook/Areas/Customer/Controllers/CartController.cs
+++ BulkyBook/Areas/Customer/Controllers/CartController.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBook.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class CartController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
