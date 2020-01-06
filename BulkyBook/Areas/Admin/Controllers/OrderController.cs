a61014d58755ef32146a68d992a8c4fcdfdfd044
Ben Spark <bhrugen1990@gmail.com>
2020-01-05 20:31:45 -0600

    Section 19 - Process Delayed Payments

--- BulkyBook/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBook/Areas/Admin/Controllers/OrderController.cs
@@ -41,6 +41,48 @@ namespace BulkyBook.Areas.Admin.Controllers
             return View(OrderVM);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Details")]
+        public IActionResult Details(string stripeToken)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == OrderVM.OrderHeader.Id,
+                                                includeProperties: "ApplicationUser");
+            if(stripeToken!=null)
+            {
+                //process the payment
+                var options = new ChargeCreateOptions
+                {
+                    Amount = Convert.ToInt32(orderHeader.OrderTotal*100),
+                    Currency = "usd",
+                    Description = "Order ID : " + orderHeader.Id,
+                    Source = stripeToken
+                };
+
+                var service = new ChargeService();
+                Charge charge = service.Create(options);
+
+                if (charge.Id == null)
+                {
+                    orderHeader.PaymentStatus = SD.PaymentStatusRejected;
+                }
+                else
+                {
+                    orderHeader.TransactionId = charge.Id;
+                }
+                if (charge.Status.ToLower() == "succeeded")
+                {
+                    orderHeader.PaymentStatus = SD.PaymentStatusApproved;
+                   
+                    orderHeader.PaymentDate = DateTime.Now;
+                }
+
+                _unitOfWork.Save();
+                
+            }
+            return RedirectToAction("Details", "Order", new { id = orderHeader.Id });
+        }
+
 
         [Authorize(Roles =SD.Role_Admin+","+SD.Role_Employee)]
         public IActionResult StartProcessing(int id)
