2a7cf5d50ca4a589de59b3f929fe6b191609c06a
Ben Spark <bhrugen1990@gmail.com>
2020-01-06 23:28:53 -0600

    Section 20 - Build Custom Tag Helper

--- BulkyBook/TagHelpers/PageLinkTagHelper.cs
+++ BulkyBook/TagHelpers/PageLinkTagHelper.cs
@@ -1,11 +1,50 @@
-﻿using System;
+﻿using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BulkyBook.TagHelpers
 {
-    public class PageLinkTagHelper
+    [HtmlTargetElement("div", Attributes ="page-model")]
+    public class PageLinkTagHelper : TagHelper
     {
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public PagingInfo PageModel { get; set; }
+
+        public string PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; }
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            TagBuilder result = new TagBuilder("div");
+
+            for(int i = 1; i < PageModel.TotalPage; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                string url = PageModel.urlParam.Replace(":", i.ToString());
+                tag.Attributes["href"] = url;
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
+                tag.InnerHtml.Append(i.ToString());
+                result.InnerHtml.AppendHtml(tag);
+            }
+
+            output.Content.AppendHtml(result.InnerHtml);
+        }
+
+
     }
 }
