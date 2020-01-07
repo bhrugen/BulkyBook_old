5ac401e1ff6a0cdbdfab68408c99eddff0f77903
Ben Spark <bhrugen1990@gmail.com>
2020-01-06 23:28:28 -0600

    Section 20 - Build Custom Tag Helper

---
+++ BulkyBook/TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.TagHelpers
+{
+    public class PageLinkTagHelper
+    {
+    }
+}
