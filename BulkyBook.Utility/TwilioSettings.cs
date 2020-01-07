aeafc7a1f641d5684116b8d6ce85cb0d2c9fd8d8
Ben Spark <bhrugen1990@gmail.com>
2020-01-06 22:18:07 -0600

    Section 20 - Setup Twilio

---
+++ BulkyBook.Utility/TwilioSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.Utility
+{
+    public class TwilioSettings
+    {
+        public string PhoneNumber { get; set; }
+        public string AuthToken { get; set; }
+        public string AccountSid { get; set; }
+    }
+}
