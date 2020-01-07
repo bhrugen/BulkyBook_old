fb5c8c839eed629d4f1173b952a4fc2a4c7edb6a
Ben Spark <bhrugen1990@gmail.com>
2020-01-07 16:17:04 -0600

    Section 20 - Process Brain Tree Payment

--- BulkyBook/Areas/Admin/Controllers/BrainTreeController.cs
+++ BulkyBook/Areas/Admin/Controllers/BrainTreeController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Braintree;
 using BulkyBook.Utility;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBook.Areas.Admin.Controllers
@@ -23,6 +25,34 @@ namespace BulkyBook.Areas.Admin.Controllers
             var clientToken = gateway.ClientToken.Generate();
             ViewBag.ClientToken = clientToken;
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(IFormCollection collection)
+        {
+            Random rnd = new Random();
+            string nonceFromtheClient = collection["payment_method_nonce"];
+            var request = new TransactionRequest
+            {
+                Amount = rnd.Next(1, 100),
+                PaymentMethodNonce = nonceFromtheClient,
+                OrderId = "55501",
+                Options = new TransactionOptionsRequest
+                {
+                    SubmitForSettlement = true
+                }
+            };
+
+            var gateway = _brain.GetGateway();
+            Result<Transaction> result = gateway.Transaction.Sale(request);
+
+            if (result.Target.ProcessorResponseText == "Approved")
+            {
+                TempData["Success"] = "Transaction was successful Transaction ID "
+                                + result.Target.Id + ", Amount Charged : $" + result.Target.Amount;
+            }
+            return RedirectToAction("Index");
         }
     }
 }
