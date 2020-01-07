663ae57bb93f5bd881ce07f2d7b3ba180c090bf1
Ben Spark <bhrugen1990@gmail.com>
2020-01-07 15:20:28 -0600

    Section 20 - Add Brain Tree Get Action

---
+++ BulkyBook/Areas/Admin/Controllers/BrainTreeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrainTreeController : Controller
+    {
+        public IBrainTreeGate _brain { get; set; }
+
+        public BrainTreeController(IBrainTreeGate brain)
+        {
+            _brain = brain;
+        }
+
+        public IActionResult Index()
+        {
+            var gateway = _brain.GetGateway();
+            var clientToken = gateway.ClientToken.Generate();
+            ViewBag.ClientToken = clientToken;
+            return View();
+        }
+    }
+}
