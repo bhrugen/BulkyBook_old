c1d00586de5da7484fab9bb6ee9550096f71f3e6
Ben Spark <bhrugen1990@gmail.com>
2020-01-06 23:10:30 -0600

    Section 20 - Custom Tag Helper Model

---
+++ BulkyBook.Models/PagingInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItem { get; set; }
+
+        public int ItemsPerPage { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public string urlParam { get; set; }
+        public int TotalPage => (int)Math.Ceiling((decimal)TotalItem / ItemsPerPage);
+    }
+}
